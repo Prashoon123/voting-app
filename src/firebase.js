ce486b01ee00acd0499f9e7e8eb5f9200e9c97cc
PrashoonB <prashoonbhattacharjee@gmail.com>
2021-03-09 16:40:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDWVKXKy6XcKp5bT08PXiBd-8WhfqrJ5GY",
-  authDomain: "voting-app-prashoon.firebaseapp.com",
-  projectId: "voting-app-prashoon",
-  storageBucket: "voting-app-prashoon.appspot.com",
-  messagingSenderId: "1023542323328",
-  appId: "1:1023542323328:web:0c17cdd258d8f74cf9de65",
-  measurementId: "G-M8C6HBMNK3",
+  // ENTER YOUR APP'S FIREBASE CONFIG HERE
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
